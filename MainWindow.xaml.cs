20c7f15a209176920eb0a58ded5865cae63895ec
p76984275 <p76984275@gmail.com>
2016-09-23 17:46:30 +0800

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace MetroWindowTitleCharacterCasing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WrongWindowBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            var wrongWindow = new WrongWindow
+            {
+                Owner = this,
+            };
+            wrongWindow.ShowDialog();
+        }
+        
+        private void CorrectWindowBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            var correctWindow = new MetroWindow()
+            {
+                Owner = this,
+                WindowStartupLocation = WindowStartupLocation.CenterOwner,
+                Title = "Correct Window",
+                Width = 400, Height = 300
+            };
+            correctWindow.ShowDialog();
+        }
+    }
+}
